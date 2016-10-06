a965f0951b5d48fa7f9c5f7c2c6119dcf0a4b834
Marjan Stojanov <marjanstojanov90@gmail.com>
2016-10-06 15:08:10 +0200

    Version 1.2.6

--- german/german_lang.php
+++ german/german_lang.php
@@ -2556,7 +2556,6 @@ $lang['smtp_encryption']= 'E-Mail Verschlüsselung';
 $lang['smtp_encryption_none']= 'Keine';
 $lang['show_proposals_on_calendar']= 'Vorschläge';
 $lang['invoice_project_see_billed_expenses']= 'Ausgaben ansehen, die auf der Rechnung integriert werden';
-$lang['recaptcha_help_settings']= 'Wenn Felder nicht befüllt sind oder eine Interneverbindung fehlt, wird das reCAPTCHA nicht verwendet.';
 $lang['project_overview_expenses'] = 'Gesamt Ausgaben';
 $lang['project_overview_expenses_billable'] = 'Verrechenbare Ausgaben';
 $lang['project_overview_expenses_billed'] = 'Verrechnete Ausgaben';
@@ -2864,3 +2863,16 @@ $lang['related_to_project'] =  'This %s is related to %s: %s';
 $lang['back_to_lead'] =  'Back to lead';
 $lang['add_task_timer_started_warning'] =  'Stop current started timer for this task to be able to add new timer manualy.';
 $lang['sending_email_contact_permissions_warning'] =  'Failed to auto select customer contacts. Please make sure that the customer have associated contacts with permission %s';
+
+# Version 1.2.6
+$lang['currently_supported_currencies'] = 'Currently supported currencies';
+$lang['authorize_notice'] = 'SSL is required if you\'re using the Authorize.Net AIM payment API. Authorize.net only supports 1 currency per account. Make sure you add only 1 currency associated with your Authorize account in the currencies field.';
+$lang['settings_paymentmethod_developer_mode'] = 'Developer Mode';
+$lang['payment_cardholder_name'] = 'Cardholder\'s Name';
+$lang['settings_paymentmethod_authorize_api_login_id'] =  'API Login ID';
+$lang['settings_paymentmethod_mollie_api_key'] =  'API Key';
+$lang['settings_paymentmethod_authorize_api_transaction_key'] =  'API Transaction ID';
+$lang['settings_paymentmethod_authorize_secret_key'] =  'Secret Key';
+$lang['leads_report_converted_notice'] =  'Only leads thats belongs in the default status Client will be taken as converted leads, if the leads belongs to the default status client and its not converted to customer will be still counted as converted lead';
+$lang['payment_method'] = 'Payment Method';
+$lang['payment_method_info'] = 'Some payment gateways support different/multiple payment methods like Credit Card, Paypal, Bank.';
