0979bb880a66142a8f413d00f50e54716909bcd5
Marjan Stojanov <marjanstojanov90@gmail.com>
2016-12-30 19:09:54 +0100

    Update swedish_lang.php

--- swedish/swedish_lang.php
+++ swedish/swedish_lang.php
@@ -3350,6 +3350,7 @@ $lang['search_ajax_searching'] = 'Searching...';
 $lang['search_ajax_initialized'] = 'Start typing to search';
 $lang['lead_description'] = 'Description';
 $lang['lead_website'] = 'Website';
+$lang['invoice_activity_auto_converted_from_estimate'] = 'Invoice auto created from estimate with number %s';
 $lang['hour_of_day_perform_auto_operations'] = 'Hour of day to perform automatic operations';
 $lang['hour_of_day_perform_auto_operations_format'] = '24 hours format eq. 9 for 9am or 15 for 3pm.';
 $lang['inv_hour_of_day_perform_auto_operations_help'] = 'Used for recurring invoices, overdue notices etc..';
