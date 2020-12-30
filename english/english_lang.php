cddff9096d4cc5e5cea369f05fb499d8a1d3d8a2
Douglas Okolaa <okolaadouglas@gmail.com>
2020-12-30 15:23:26 +0100

    upcoming version language

--- english/english_lang.php
+++ english/english_lang.php
@@ -3687,3 +3687,6 @@ $lang['something_went_wrong']            = 'Something went wrong. Try again';
 $lang['imap_folder']                     = 'Folder';
 $lang['retrieve_folders']                = 'Retrieve Folders';
 $lang['email_to_ticket_config']          = 'Email to ticket configuration';
+
+# Version 2.7.4
+$lang['enable_support_menu_badges']          = 'Enable Support menu item badge';
